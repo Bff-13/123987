4dcc74e72cff4233e23a2c5aea0b2e83161e836f
Bff-13 <1728085849@qq.com>
2020-10-15 13:33:29 +0800

    test

--- ex.py
+++ ex.py
@@ -1,15 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-t = np.linspace(-10.0,10.0,1000)  
+t = np.linspace(-10,10)  
 plt.ylim(0,5)                          
 plt.subplot(221)
-plt.title(u'exp(-0.6*t)')
-ft = np.exp(-0.6*t)          
+plt.title(u'exp(-0.4*t)')
+ft = np.exp(-0.4*t)          
 plt.plot(t,ft) 
 plt.subplot(222)
-plt.title(u'exp(0.6*t)')
-ft1 = np.exp(0.6*t)
+plt.title(u'exp(0.4*t)')
+ft1 = np.exp(0.4*t)
 plt.plot(t,ft1)
 plt.subplot(212)
 plt.title(u'exp(0*t)')
---
+++ 幅度减小的正弦函数.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif']=['SimHei']
+plt.rcParams['axes.unicode_minus']=False
+t=np.linspace(-3.0,3.0,1000)
+plt.ylim(0,8)
+f=2*np.exp((complex(-0.8,8))*t)
+plt.subplot(221)
+plt.title(u'幅度减小的正弦信号')
+plt.plot(t,f)
+plt.show()
