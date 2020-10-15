6c98633f695f30631faae303105da843a264a6b0
Bff-13 <1728085849@qq.com>
2020-10-15 08:49:20 +0800

    commit

--- ex.py
+++ ex.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-t = np.linspace(-10.0,10.0,1000)  #通过linspace函数指定t的取值范围
-plt.ylim(0,5)                          #定义纵轴取值范围
-plt.subplot(221)     #显示的位置
-plt.title(u'exp(-0.6*t)')   #图名
-ft = np.exp(-0.6*t)              #调用exp函数计算指数信号
-plt.plot(t,ft)                #绘图
+t = np.linspace(-10.0,10.0,1000)  
+plt.ylim(0,5)                          
+plt.subplot(221)
+plt.title(u'exp(-0.6*t)')
+ft = np.exp(-0.6*t)          
+plt.plot(t,ft) 
 plt.subplot(222)
 plt.title(u'exp(0.6*t)')
 ft1 = np.exp(0.6*t)
